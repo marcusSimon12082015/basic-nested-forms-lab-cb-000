d8c27f7c56b66b15eacc082c40a460ace7b90ca8
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-15 08:22:50 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients)
+    ingredients.each do |i,ingredient_attributes|
+      self.ingredients.build(ingredient_attributes)
+    end
+  end
 end
